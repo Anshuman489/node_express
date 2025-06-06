e97ea02c730ef63d511d52d46d83b133e27c5089
Anshuman489 <anshumanpadhi05@gmail.com>
2025-06-06 20:06:24 +0530

    transferred code

--- 07_mind_grenade.js
+++ 07_mind_grenade.js
@@ -1,8 +1,51 @@
-const num1 = 5
-const num2 = 10
+// const num1 = 5
+// const num2 = 10
 
-function addValues(){
-  console.log(`the sum is ${num1+num2}`)
-}
+// function addValues(){
+//   console.log(`the sum is ${num1+num2}`)
+// }
 
-addValues()
+// addValues()
+
+
+
+//callback hell fix
+
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS is AWESOME : \n${first}\n${second}\n\n`
+      // { flag: "a" }
+    );
+    console.log(first);
+    console.log(second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
